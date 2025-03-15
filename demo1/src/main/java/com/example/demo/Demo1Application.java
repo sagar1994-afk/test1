5cb3593347a6152e6dfdb3b206d8be54b9b4a5e3
Neha <Neha@DESKTOP-1ALP904>
2025-03-15 21:18:39 +0530

    first commit

--- demo1/src/main/java/com/example/demo/Demo1Application.java
+++ demo1/src/main/java/com/example/demo/Demo1Application.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Demo1Application {
 
 	public static void main(String[] args) {
-		System.out.println("hello");
+		System.out.println("hello1");
 		SpringApplication.run(Demo1Application.class, args);
 	}
 
